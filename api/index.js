e665bd491a561f1e44f2c640f1a13691069044e7
Jay Prakash Pathak <jayprkshpthk@gmail.com>
2025-10-04 11:37:31 +0530

    fix build

--- api/index.js
+++ api/index.js
@@ -1,12 +1,14 @@
-const path = require("path");
-const express = require("express");
-const compression = require("compression");
+import compression from "compression";
+import express from "express";
+import fs from "fs";
+import path from "path";
+import { fileURLToPath, pathToFileURL } from "url";
 
-const isTest = process.env.NODE_ENV === "test";
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 async function createServer(root = process.cwd()) {
   const resolve = (p) => path.resolve(__dirname, p);
-  const indexProd = require("fs").readFileSync(
+  const indexProd = fs.readFileSync(
     resolve("../dist/client/index.html"),
     "utf-8"
   );
@@ -22,7 +24,10 @@ async function createServer(root = process.cwd()) {
   app.use(async (req, res) => {
     try {
       const url = req.originalUrl;
-      const render = require(resolve("../dist/server/entry-server.js")).render;
+      const mod = await import(
+        pathToFileURL(resolve("../dist/server/entry-server.js")).href
+      );
+      const render = mod.render;
       const appHtml = render(url);
       const html = indexProd.replace(`<!--ssr-outlet-->`, appHtml);
       res.status(200).set({ "Content-Type": "text/html" }).end(html);
@@ -44,7 +49,7 @@ function getApp() {
   return appPromise;
 }
 
-module.exports = async (req, res) => {
+export default async (req, res) => {
   const app = await getApp();
   return app(req, res);
 };
